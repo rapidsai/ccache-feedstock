31c884a48e707c9c61103750edfae3f5d603389b
Aleksander Ficek <alex.ficek99@gmail.com>
2020-07-28 15:12:38 -0700

    Add -c conda-forge

--- ci/gpuci/run.sh
+++ ci/gpuci/run.sh
@@ -10,5 +10,5 @@ export PATH="/opt/conda/bin:$PATH"
 source activate base
 conda config --set ssl_verify false
 conda install -k conda-build conda-verify anaconda-client
-conda build recipe
+conda build -c conda-forge recipe
 anaconda -t ${MY_UPLOAD_KEY} upload -u ${CONDA_USERNAME:-gpuci} --label main --skip-existing /opt/conda/conda-bld/linux-64/rapids-ccache*.tar.bz2
