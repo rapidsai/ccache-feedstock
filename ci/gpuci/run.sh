20259ee46acdf6aa6255e70c193e694a71926833
Aleksander Ficek <alex.ficek99@gmail.com>
2020-07-28 15:10:03 -0700

    Add run.sh script

---
+++ ci/gpuci/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Copyright (c) 2020, NVIDIA CORPORATION.
+####################################
+# ccache fork run script #
+####################################
+set -ex
+
+export HOME=${WORKSPACE}
+export PATH="/opt/conda/bin:$PATH"
+source activate base
+conda config --set ssl_verify false
+conda install -k conda-build conda-verify anaconda-client
+conda build recipe
+anaconda -t ${MY_UPLOAD_KEY} upload -u ${CONDA_USERNAME:-gpuci} --label main --skip-existing /opt/conda/conda-bld/linux-64/rapids-ccache*.tar.bz2
