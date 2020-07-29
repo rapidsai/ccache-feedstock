eea9e289fdb036c596f57b94f1e7f6693f59ead0
Aleksander Ficek <alex.ficek99@gmail.com>
2020-07-29 08:25:08 -0700

    Update package name

--- ci/gpuci/run.sh
+++ ci/gpuci/run.sh
@@ -11,4 +11,4 @@ source activate base
 conda config --set ssl_verify false
 conda install -k conda-build conda-verify anaconda-client
 conda build -c conda-forge recipe
-anaconda -t ${MY_UPLOAD_KEY} upload -u ${CONDA_USERNAME:-gpuci} --label main --skip-existing /opt/conda/conda-bld/linux-64/rapids-ccache*.tar.bz2
+anaconda -t ${MY_UPLOAD_KEY} upload -u ${CONDA_USERNAME:-gpuci} --label main --skip-existing /opt/conda/conda-bld/linux-64/gpuci-ccache*.tar.bz2
